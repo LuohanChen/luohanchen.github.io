80d2e910db628a475c74cc0f122338592a8948cf
LuohanChen <130841659+LuohanChen@users.noreply.github.com>
2024-04-18 11:30:02 +1000

    javascript practice in class

---
+++ week5/script.js
@@ -0,0 +1,35 @@
+// let myName = "Loochan";
+// let myName = prompt("what is your name?");
+// console.log("Hello, ", myName);
+
+// let studentNames = ["Rohit", "Loochan", "Mav"];
+// console.log(studentNames[0]);
+// console.log(studentNames[1]);
+// console.log(studentNames[2]);
+
+// let studentRecord = [
+    // {name: "Rohit", id: 2, marks: 30},
+    // {name: "Thomas", id: 4, marks: 35},
+    // {name: "Cyrian", id: 5, marks: 40},
+   // {name: "Jonathan-Ericson", id: 6, marks: 12}
+// ]; 
+
+// console.log(studentRecord[1].name + " got the following marks: " + studentRecord[1].marks);
+
+let temperature = prompt("What is the temperature today?");
+if (temperature>=20 && temperature <30)
+{
+    console.log("it feels sunny and warm");
+}
+else if (temperature>=10 && temperature<20)
+{
+console.log ("it feels cold");
+}
+else if (temperature>30)
+{
+    console.log ("wa damn hot leh");
+}
+else if (temperature<10)
+{
+    console.log ("it is freezing")
+}
