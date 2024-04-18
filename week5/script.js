722b8646de15572941056698c3b5d93b716d03eb
LuohanChen <130841659+LuohanChen@users.noreply.github.com>
2024-04-18 12:21:53 +1000

    Javascript practice

--- week5/script.js
+++ week5/script.js
@@ -16,20 +16,55 @@
 
 // console.log(studentRecord[1].name + " got the following marks: " + studentRecord[1].marks);
 
-let temperature = prompt("What is the temperature today?");
-if (temperature>=20 && temperature <30)
+let tempInput = document.querySelector("#temperature");
+
+let temperature = tempInput.value;
+
+
+// let temperature = prompt("What is the temperature today?");
+function checkWeather()
 {
-    console.log("it feels sunny and warm");
+    if (temperature>=20 && temperature<30)
+    {
+        console.log("it feels sunny and warm");
+    }
+    else if (temperature>=10 && temperature<20)
+    {
+    console.log ("it feels cold");
+    }
+    else if (temperature>30)
+    {
+        console.log ("wa damn hot leh");
+    }
+    else if (temperature<10)
+    {
+        console.log ("it is freezing")
+    }
 }
-else if (temperature>=10 && temperature<20)
-{
-console.log ("it feels cold");
+
+let shoppingCart = [
+    { name: "T-shirt", price: 20 },
+    { name: "Jeans", price: 50 },
+    { name: "sneakers", price: 120},
+    { name: "backpack", price: 30}
+]
+
+function calculateTotal() {
+    let total = 0;
+    for (let i = 0; i < 4; i++) {
+        total = total + shoppingCart[i].price;
+        console.log("the sum so far", total);
+    }
+    console.log("total price", total);
+    let discount = 0.1;
+    let discountedPrie = 0;
+    if (total>100) {
+        discountedPrice = total - total * discount;
+    }
+    
+    console.log ("the discounted price", discountedPrice);
 }
-else if (temperature>30)
-{
-    console.log ("wa damn hot leh");
-}
-else if (temperature<10)
-{
-    console.log ("it is freezing")
-}
+
+// let val = calculateTotal();
+let val = calculateTotal(0.5);
+console.log(val);
