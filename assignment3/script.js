e74d44ce15181098e5e4cd1cb32389ed80e48251
LuohanChen <130841659+LuohanChen@users.noreply.github.com>
2024-06-13 13:05:27 +1000

    Assignment 3 progress

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,14 +1,29 @@
 // Needed functionalities (JS):
 // random selection button, fortune cookie onclick change image, fortune cookie randomiser button, canvas scroll in/out, canvas draggable
 
-const zoomElement = document.querySelector(".cookies");
-let zoom = 1;
-const ZOOM_SPEED = 0.1;
+const zoomElement = document.querySelector("canvas");
 
-document.addEventListener("wheel", function (e) {
-  if (e.deltaY > 0) {
-    zoomElement.style.transform = `scale(${(zoom += ZOOM_SPEED)})`;
-  } else {
-    zoomElement.style.transform = `scale(${(zoom -= ZOOM_SPEED)})`;
-  }
+let currentZoom = 1;
+let minZoom = 0.3;
+let maxZoom = 3;
+let stepSize = 0.05;
+
+canvas.addEventListener("wheel", function (event) {
+  let direction = event.deltaY > 0 ? -1 : 1;
+  zoomImage(direction);
 });
+
+function zoomImage(direction) {
+  let newZoom = currentZoom + direction * stepSize;
+
+  // Limit the zoom level to the minimum and maximum values
+  if (newZoom < minZoom || newZoom > maxZoom) {
+    return;
+  }
+
+  currentZoom = newZoom;
+
+  // Update the CSS transform of the image to scale it
+  let image = document.querySelector("#canvas");
+  image.style.transform = "scale(" + currentZoom + ")";
+}
