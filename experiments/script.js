2c8d0d8e678de58635264177b090c09b4bed35a2
LuohanChen <130841659+LuohanChen@users.noreply.github.com>
2024-06-16 00:55:22 +1000

    PROGRESS!!

--- experiments/script.js
+++ experiments/script.js
@@ -1,7 +1,26 @@
-var cards = document.querySelectorAll(".column");
+function toggleStyle() {
+  var element = document.getElementById("cardContainer");
+  element.classList.toggle("is-clicked");
 
-[...cards].forEach((card) => {
-  card.addEventListener("click", function () {
-    card.classList.toggle("is-flipped");
-  });
-});
+  setTimeout(function () {
+    element.classList.remove("is-clicked");
+  }, 1000);
+}
+
+var modal = document.getElementById("modal");
+var open = document.getElementById("btn");
+var span = document.getElementsByClassName("close")[0];
+
+open.onclick = function () {
+  modal.style.display = "flex";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
