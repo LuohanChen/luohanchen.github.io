d8ff7b498e2d92f88b5a8063c26b7ab3c5d405a2
LuohanChen <130841659+LuohanChen@users.noreply.github.com>
2024-06-16 05:22:37 +1000

    updated js

--- experiments/script.js
+++ experiments/script.js
@@ -45,30 +45,28 @@ window.addEventListener("click", function (event) {
   });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Card tilt while cursor is hovering over the page (elaborate)
-  const cards = document.querySelectorAll(".modalcard");
-  let rect;
+// Card tilt while cursor is hovering over the page (elaborate)
+const cards = document.querySelectorAll(".modalcard");
+let rect;
 
+cards.forEach((card) => {
+  rect = card.getBoundingClientRect();
+});
+
+document.addEventListener("mousemove", function (e) {
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+  let tiltX = ((x - centerX) / centerX) * -5;
+  let tiltY = ((y - centerY) / centerY) * -10;
+
+  tiltX = Math.min(Math.max(tiltX, -30), 30);
+  tiltY = Math.min(Math.max(tiltY, -30), 30);
+
+  // Apply transformation to each card individually
   cards.forEach((card) => {
-    rect = card.getBoundingClientRect();
-  });
-
-  document.addEventListener("mousemove", function (e) {
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-    let tiltX = ((x - centerX) / centerX) * -5;
-    let tiltY = ((y - centerY) / centerY) * -10;
-
-    tiltX = Math.min(Math.max(tiltX, -30), 30);
-    tiltY = Math.min(Math.max(tiltY, -30), 30);
-
-    // Apply transformation to each card individually
-    cards.forEach((card) => {
-      card.style.transform = `rotateX(${tiltY}deg) rotateY(${tiltX}deg)`;
-      card.style.boxShadow = `${tiltX}px ${tiltY}px 30px rgb(249, 210, 122, 0.3)`;
-    });
+    card.style.transform = `rotateX(${tiltY}deg) rotateY(${tiltX}deg)`;
+    card.style.boxShadow = `${tiltX}px ${tiltY}px 30px rgb(249, 210, 122, 0.3)`;
   });
 });
