bfc4a4738a40b48897aeadb68e80a053d00d3939
LuohanChen <130841659+LuohanChen@users.noreply.github.com>
2025-04-02 12:37:48 +1100

    Feedback experiments

--- dms5/feedbackExercise/script.js
+++ dms5/feedbackExercise/script.js
@@ -9,13 +9,31 @@ let currentVolume = null;
 setInterval(() => {
   /* meter defined in toneSetup */
   currentVolume = meter.getValue();
-  // let newValue = clamp(currentVolume, -48, 0);
-  // let newValueInt = parseInt(newValue);
-  // let newRemappedValue = remapRange(newValueInt, -48, -6, 0, 1);
-  // let newRemappedValueInt = parseFloat(newRemappedValue.toPrecision(2));
+  let newValue = clamp(currentVolume, -48, 0);
+  let newValueInt = parseInt(newValue);
+  let newRemappedValue = remapRange(newValueInt, -48, -6, 0, 100);
+  let newRemappedValueInt = parseFloat(newRemappedValue.toPrecision(2));
   // meterText.innerHTML = newRemappedValueInt + "%";
+  if (newRemappedValueInt < 1) {
+meterText.innerHTML = "🔇";
+  } else if (newRemappedValueInt < 60) {
+    meterText.innerHTML = "🔉";
+  }
+ else {
+  meterText.innerHTML = "🔊";
+ }
 
-  remappedValue =  parseInt(remapRange(clamp(currentVolume, -48, 0), -48, -6, 0, 100));
-  meterText.innerHTML = `${remappedValue} %`;
+ console.log(newRemappedValueInt);
+
+  // getComputedStyle(document.documentElement).setProperty("--col03", "red");
+
+  // let newColour = "color-mix(in hsl, hsl(200 50 80), coral 80%)";
+
+  let newColour = `color-mix(in hsl, red, blue ${newRemappedValueInt}%)`;
+  console.log(newColour);
+  document.getElementById("playbackToggle").style.backgroundColor = newColour;
+
+  // remappedValue =  parseInt(remapRange(clamp(currentVolume, -48, 0), -48, -6, 0, 100));
+  // meterText.innerHTML = `${remappedValue} %`; 
 }, meterCheckInterval);
 
