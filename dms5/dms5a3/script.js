53393ccaad66bca1591cc8923d556560cf2518b4
LuohanChen <130841659+LuohanChen@users.noreply.github.com>
2025-04-25 18:57:52 +1000

    added button to change what shape is selected, colour change to shapes

--- dms5/dms5a3/script.js
+++ dms5/dms5a3/script.js
@@ -1,4 +1,64 @@
-let shapeSelector = document.getElementsByClassName("changeShape");
+let currentColour = 'red';
+let currentShape = 'circle';
 
-let shapeSlectorArray = Array.from(shapeSelector);
+function createShape(pos) {
+    let shape;
+    
+    switch(currentShape) {
+        case 'circle':
+            shape = new Konva.Circle({
+                x: pos.x,
+                y: pos.y,
+                fill: currentColour,
+                radius: 20,
+                draggable: true
+            });
+            break;
+            
+        case 'square':
+            shape = new Konva.Rect({
+                x: pos.x,
+                y: pos.y,
+                fill: currentColour,
+                width: 30,
+                height: 30,
+                draggable: true
+            });
+            break;
+            
+        case 'triangle':
+            shape = new Konva.RegularPolygon({
+                x: pos.x,
+                y: pos.y,
+                fill: currentColour,
+                sides: 3,
+                radius: 20,
+                draggable: true
+            });
+            break;
+    }
+    
+    group.add(shape);
+    layer.draw();
+}
 
+stage.on('click', function() {
+    var pos = group.getRelativePointerPosition();
+    createShape(pos);
+});
+
+document.getElementById('shape1').addEventListener('click', function() {
+    currentShape = 'circle';
+});
+document.getElementById('shape2').addEventListener('click', function() {
+    currentShape = 'square';
+});
+document.getElementById('shape3').addEventListener('click', function() {
+    currentShape = 'triangle';
+});
+
+function changeCurrentColour(e) {
+    currentColour = e.target.value;
+}
+
+document.getElementById("colourPicker").addEventListener("change", changeCurrentColour);
